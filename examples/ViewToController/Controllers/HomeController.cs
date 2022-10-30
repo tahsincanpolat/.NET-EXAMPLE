188936ce949b19fb4a6e8009641533088e52de61
tahsincanpolat <tahsincanpolat@gmail.com>
2022-10-30 18:33:00 +0300

    Controller To View - Post Form

---
+++ examples/ViewToController/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ViewToController.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(string kisiler,string ad,bool onay)
+        {
+            var k1 = Request.Form["kisiler"];
+            var a1 = Request.Form["ad"];
+            var o1 = Request.Form.GetValues("onay")[0];
+            return View();
+        }
+    }
+}
