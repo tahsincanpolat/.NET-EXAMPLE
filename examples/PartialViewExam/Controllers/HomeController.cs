5823fbecab42cd12ef0b11a50e192386d3a7f4af
tahsincanpolat <tahsincanpolat@gmail.com>
2022-10-30 17:05:58 +0300

    partial views

---
+++ examples/PartialViewExam/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PartialViewExam.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        // Partial View => Kısmi görünümler isteğimiz bir view dosyasını istediğimiz alanda component mantığında kullanmak için oluşturulur.
+        public ActionResult HomePage()
+        {
+            return View();
+        }
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
